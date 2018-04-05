0c5078adf2151f750fe8a2f5559c625c242d57b3
Mark Johnson <mfjohnson98@yahoo.com>
2018-04-04 22:54:56 -0400

    reduced count amt

--- spark_hdfs/hdfs_spark_write.py
+++ spark_hdfs/hdfs_spark_write.py
@@ -5,7 +5,7 @@ warehouseLocation="/"
 spark = SparkSession.builder.appName("SparkSessionHDFSExample").config("spark.sql.warehouse.dir", warehouseLocation).enableHiveSupport().getOrCreate()
 
 sc = spark.sparkContext
-values = sc.parallelize(range(1,1000))
+values = sc.parallelize(range(1,10))
 values.saveAsTextFile("hdfs://hdfs/testfile")
 
 #inp_file = sc.textFile("hdfs://")
